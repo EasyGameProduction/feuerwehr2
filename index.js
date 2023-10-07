957be7d8599ada6c7397bf479a1e8de136565c2f
EasyGameProduction <80057008+EasyGameProduction@users.noreply.github.com>
2023-10-07 16:33:02 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,320 @@
+var fragen = [[// Frage 1
+               "Ihr Trupp erhält den Auftrag, für den Angriffstrupp eine Schlauchleitung zum Vorgehen in ein Gebäude vorzubereiten. Einsatzziel ist eine Wohnung im 2. Obergeschoss. Wie viel Schlauchreserve ordnen sie als Truppführer an?",
+               // Antwort 1
+               "4 C-Schläuche (1 C-Länge zum Gebäude, 1 C-Länge für jedes Geschoss, 1 C-Länge je Nutzungseinheit",
+               // Antwort 2
+               "5 C-Schläuche (2 C-Länge zum Gebäude, 1 C-Länge für jedes Geschoss, 1 C-Länge je Nutzungseinheit",
+               // Antwort 3
+               "6 C-Schläuche (2 C-Länge zum Gebäude, 1 C-Länge für jedes Geschoss, 2 C-Länge je Nutzungseinheit",
+               // Antwort 4
+               "1 C-Schlauch",
+               [true,false,false,false]],
+
+               [// Frage 2
+               "Im Rahmen einer Bewegungsfahrt kommen Sie an einem Verkehrsunfall vorbei. Ordnen Sie die Begriffe zum Umgang mit Verletzten bei Verkehrsunfällen in der richtigen Reihenfolge: <br>1 - Unfallstelle absichern <br>2 - Nachalarmierung veranlassen <br>3 - Zündquellen fernhalten (Handy, Feuerzeug) <br>4 - Verletzte betreuen",
+               // Antwort 1
+               "1-2-3-4",
+               // Antwort 2
+               "1-4-3-2",
+               // Antwort 3
+               "4-3-2-1",
+               // Antwort 4
+               "2-1-3-4",
+               [true,false,false,false]],
+
+               [// Frage 3
+               "Was müssen Sie bei einem Stromunfall im Niederspannungsbereich (Spannung 230 bis 400 Volt) zuerst tun? Ordnen Sie die Begriffe in der richtigen Reihenfolge! <br>1 - Den Verunfallten von einem gut isolierten Standort aus wegziehen <br>2 - Die Stromleitung spannungsfrei schalten (lassen) <br>3 - Rettungsdienst über den Einheitsführer alarmieren <br> 4 - Erste Hilfe - Maßnahmen durchführen",
+               // Antwort 1
+               "2-1-3-4",
+               // Antwort 2
+               "1-2-3-4",
+               // Antwort 3
+               "4-3-2-1",
+               // Antwort 4
+               "2-4-3-1",
+               [true,false,false,false]],
+
+               [// Frage 4
+               "Welche Aussagen über Kohlenmonoxid sind richtig?",
+               // Antwort 1
+               "entsteht bei unvollkommener Verbrennung",
+               // Antwort 2
+               "brennbar",
+               // Antwort 3
+               "leichter als Luft",
+               // Antwort 4
+               "giftig (Blut- und Nervengift)",
+               [true,true,true,true]],
+
+               [// Frage 5
+               "Welche Aussagen über Kohlenmonoxid sind richtig?",
+               // Antwort 1
+               "schwerer als Luft",
+               // Antwort 2
+               "farb-, geruch- und geschmacklos",
+               // Antwort 3
+               "ungifig",
+               // Antwort 4
+               "entsteht bei vollkommener Verbrennung",
+               [false,true,false,false]],
+
+               [// Frage 6
+               "Während des Löschangriffs (Außenangriff) entdecken Sie eine leblose Person. Welchen der folgenden Schritte führen Sie zusammen mit ihrem Truppmann im Rahmen der Patientenkontrolle aus Sicht der Ersten Hilfe vorrangig durch?",
+               // Antwort 1
+               "Zuerst Bewusstsein, dann Atmung kontrollieren",
+               // Antwort 2
+               "Beine und Arme hochlagern",
+               // Antwort 3
+               "Zuerst Blutung stillen, dann Atmung kontrollieren",
+               // Antwort 4
+               "Zuerst Feuer löschen, dann Verletzten betreuen",
+               [true,false,false,false]],
+
+               [// Frage 7
+               "Welche Aussagen über Kohlendioxid sind zutreffend?",
+               // Antwort 1
+               "entsteht bei unvollkommener Verbrennung",
+               // Antwort 2
+               "brennbar",
+               // Antwort 3
+               "leichter als Luft",
+               // Antwort 4
+               "steuert die Atmung, ist bei zu hoher Konzentration giftig",
+               [false,false,false,true]],
+
+               [// Frage 8
+               "Welche Aussagen über Kohlendioxid sind zutreffend?",
+               // Antwort 1
+               "schwerer als Luft",
+               // Antwort 2
+               "farb-, geruch- und geschmacklos",
+               // Antwort 3
+               "ungifig",
+               // Antwort 4
+               "entsteht bei vollkommener Verbrennung",
+               [true,true,false,true]],
+
+               [// Frage 9
+               "Welche Gefahren entstehen in dieser Situation? (Bild zeigt: Feuerwehrmänner tragen Pumpe in Richtung eines Flusses (Gelender vor ihnen)",
+               // Antwort 1
+               "Ausrutschen",
+               // Antwort 2
+               "Stolpern",
+               // Antwort 3
+               "Quetschen",
+               // Antwort 4
+               "Stürzen",
+               [true,true,true,true]],
+
+               [// Frage 10
+               "Kreuzen Sie die zur GAMS-Regel gehörenden Begriffe an",
+               // Antwort 1
+               "Motoren warten",
+               // Antwort 2
+               "Absperren, absichern",
+               // Antwort 3
+               "Alarmieren",
+               // Antwort 4
+               "Grundsätze erklären",
+               [false,true,false,false]],
+
+               [// Frage 11
+               "Kreuzen Sie die zur GAMS-Regel gehörenden Begriffe an",
+               // Antwort 1
+               "Spezialkräfte anfordern",
+               // Antwort 2
+               "Spitzenkräfte anfordern",
+               // Antwort 3
+               "Gefahr erkennen",
+               // Antwort 4
+               "Menschenrettung",
+               [true,false,true,true]],
+
+               [// Frage 12
+               "Sie erhalten als Truppführer den Auftrag, eine Saugleitung aufzubauen. Warum soll in fließenden Gewassern der Saugkorb im Regelfall gegen die Fließrichtung des Wassers liegen? Kreuzen Sie die richtigen Antworten an.",
+               // Antwort 1
+               "In dieser Stellung wird die Bildung eines Wasserstrudels (Luftsogs) und damit Ansaugschwierigkeiten vermieden",
+               // Antwort 2
+               "Damit die Pumpe besser ansaugt, weil das Wasser in den Saugkorb gedrückt wird",
+               // Antwort 3
+               "Die Saugleitung kann in dieser Stellung besser gehalten werden",
+               // Antwort 4
+               "Damit der Schmutz am Saugkorb leichter weggespült wird",
+               [true,true,false,false]],
+
+               [// Frage 13
+               "Worauf achten Sie bei der Entnahme von Geräten aus den Fahrzeug-Geräteräumen?",
+               // Antwort 1
+               "Dass die Geräteschlitten zur schnelleren Wiederbeladung im ausgezogenen Zustand belassen werden",
+               // Antwort 2
+               "Dass schwere Geräte von mehreren Einsatzkräften getragen werden",
+               // Antwort 3
+               "Dass die Aufstiegshilfen (Haltegriffe, Trittbretter, Leitern usw.) benutzt werden",
+               // Antwort 4
+               "Auf Verletzungsgefahren",
+               [false,true,true,true]],
+
+               [// Frage 14
+               "Welche der folgenden Feuerlöscher können bei Bränden fester Stoffe wirkungsvoll eingesetzt werden?",
+               // Antwort 1
+               "Kohlendioxidlöscher",
+               // Antwort 2
+               "Feuerlöscher mit Metallbrandpulver",
+               // Antwort 3
+               "Pulverlöscher mit Glutbrandpulver",
+               // Antwort 4
+               "Pulverlöscher mit Flammbrandpulver",
+               [false,false,true,false]],
+            ];
+
+var index = 0;
+var fehler = 0;
+
+function startGame(){
+    index = 0;
+    fehler = 0;
+    fragen = this.shuffle(fragen);
+    document.getElementById("Aufgabe").innerHTML = "Aufgabe: 1" + "/" + fragen.length.toString();
+    setFields();
+}
+this.startGame();
+
+function setFields(){
+    document.getElementById("Antwort1").style.backgroundColor = "white";
+    document.getElementById("Antwort2").style.backgroundColor = "white";
+    document.getElementById("Antwort3").style.backgroundColor = "white";
+    document.getElementById("Antwort4").style.backgroundColor = "white";
+    document.getElementById("buttonWeiter").hidden = true;
+    document.getElementById("buttonAbschicken").hidden = false;
+    document.getElementById("Frage").innerHTML = fragen[index][0];
+    document.getElementById("checkbox1Label").innerHTML = fragen[index][1];
+    document.getElementById("checkbox2Label").innerHTML = fragen[index][2];
+    document.getElementById("checkbox3Label").innerHTML = fragen[index][3];
+    if(fragen[index][4] != ""){
+        document.getElementById("checkbox4").hidden = false;
+        document.getElementById("checkbox4Label").hidden = false;
+        document.getElementById("checkbox4Label").innerHTML = fragen[index][4];
+    } else{
+        document.getElementById("checkbox4").hidden = true;
+        document.getElementById("checkbox4Label").hidden = true;
+    }
+}
+
+function checkAnswer(){
+    var falsch = false;
+    for(let i = 0; i < 4; i++){
+        if(document.getElementById("checkbox" + (+i + 1).toString()).checked ==  fragen[index][5][i]){
+            document.getElementById("Antwort" + (+i + 1).toString()).style.backgroundColor = "green";
+        } else{
+            falsch = true;
+            document.getElementById("Antwort" + (+i + 1).toString()).style.backgroundColor = "red";
+        }
+    }
+    if(falsch){
+        fehler++;
+    }
+    document.getElementById("buttonWeiter").hidden = false;
+    document.getElementById("buttonAbschicken").hidden = true;
+}
+
+function next(){
+    if(index < fragen.length -1){
+        index++;
+        document.getElementById("checkbox1").checked = false;
+        document.getElementById("checkbox2").checked = false;
+        document.getElementById("checkbox3").checked = false;
+        document.getElementById("checkbox4").checked = false;
+        document.getElementById("Aufgabe").innerHTML = "Aufgabe: " + (+index + +1).toString() + "/" + fragen.length.toString();
+        setFields();
+    } else{
+        if(fehler == 0){
+            Swal.fire({
+                title: 'Fehlerfrei',
+                text: 'Du hast alle ' + fragen.length.toString() + ' richtig beantwortet.',
+                icon: 'success',
+                timerProgressBar: true,
+                timer: 4000,
+                didOpen: () => {Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                  b.textContent = Swal.getTimerLeft()
+                }, 100)
+              },
+              willClose: () => {
+                clearInterval(timerInterval)
+              }
+            }).then((result) => {
+              /* Read more about handling dismissals below */
+              if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('I was closed by the timer')
+              }
+            })
+        } else if(fehler <= 2){
+            Swal.fire({
+                title: 'Naja',
+                text: 'Du hast ' + (+fragen.length - +fehler).toString() + ' von ' + fragen.length.toString() + ' richtig beantwortet.',
+                icon: 'warning',
+                timerProgressBar: true,
+                timer: 4000,
+                didOpen: () => {Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                  b.textContent = Swal.getTimerLeft()
+                }, 100)
+              },
+              willClose: () => {
+                clearInterval(timerInterval)
+              }
+            }).then((result) => {
+              /* Read more about handling dismissals below */
+              if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('I was closed by the timer')
+              }
+            })
+        } else if(fehler >= 3){
+            Swal.fire({
+                title: 'Übungsbedarf',
+                text: 'Du hast nur ' + (+fragen.length - +fehler).toString() + ' von ' + fragen.length.toString() + ' richtig beantwortet.',
+                icon: 'error',
+                timerProgressBar: true,
+                timer: 4000,
+                didOpen: () => {Swal.showLoading()
+                const b = Swal.getHtmlContainer().querySelector('b')
+                timerInterval = setInterval(() => {
+                  b.textContent = Swal.getTimerLeft()
+                }, 100)
+              },
+              willClose: () => {
+                clearInterval(timerInterval)
+              }
+            }).then((result) => {
+              /* Read more about handling dismissals below */
+              if (result.dismiss === Swal.DismissReason.timer) {
+                console.log('I was closed by the timer')
+              }
+            })
+        }
+        this.startGame();
+    }
+}
+
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
