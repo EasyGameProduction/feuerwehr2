f6bd4583d139e234e079d6ef33edcfc2dce1cce5
EasyGameProduction <80057008+EasyGameProduction@users.noreply.github.com>
2023-10-08 12:16:28 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ var fragen = [[// Frage 1
                "6 C-Schläuche (2 C-Länge zum Gebäude, 1 C-Länge für jedes Geschoss, 2 C-Länge je Nutzungseinheit",
                // Antwort 4
                "1 C-Schlauch",
-               [true,false,false,false]],
+               [true,false,false,false],
+               // Bild
+               ""],
 
                [// Frage 2
                "Im Rahmen einer Bewegungsfahrt kommen Sie an einem Verkehrsunfall vorbei. Ordnen Sie die Begriffe zum Umgang mit Verletzten bei Verkehrsunfällen in der richtigen Reihenfolge: <br>1 - Unfallstelle absichern <br>2 - Nachalarmierung veranlassen <br>3 - Zündquellen fernhalten (Handy, Feuerzeug) <br>4 - Verletzte betreuen",
@@ -20,7 +22,9 @@ var fragen = [[// Frage 1
                "4-3-2-1",
                // Antwort 4
                "2-1-3-4",
-               [true,false,false,false]],
+               [true,false,false,false],
+               // Bild
+               ""],
 
                [// Frage 3
                "Was müssen Sie bei einem Stromunfall im Niederspannungsbereich (Spannung 230 bis 400 Volt) zuerst tun? Ordnen Sie die Begriffe in der richtigen Reihenfolge! <br>1 - Den Verunfallten von einem gut isolierten Standort aus wegziehen <br>2 - Die Stromleitung spannungsfrei schalten (lassen) <br>3 - Rettungsdienst über den Einheitsführer alarmieren <br> 4 - Erste Hilfe - Maßnahmen durchführen",
@@ -32,7 +36,9 @@ var fragen = [[// Frage 1
                "4-3-2-1",
                // Antwort 4
                "2-4-3-1",
-               [true,false,false,false]],
+               [true,false,false,false],
+               // Bild
+               ""],
 
                [// Frage 4
                "Welche Aussagen über Kohlenmonoxid sind richtig?",
@@ -44,7 +50,9 @@ var fragen = [[// Frage 1
                "leichter als Luft",
                // Antwort 4
                "giftig (Blut- und Nervengift)",
-               [true,true,true,true]],
+               [true,true,true,true],
+               // Bild
+               ""],
 
                [// Frage 5
                "Welche Aussagen über Kohlenmonoxid sind richtig?",
@@ -56,7 +64,9 @@ var fragen = [[// Frage 1
                "ungifig",
                // Antwort 4
                "entsteht bei vollkommener Verbrennung",
-               [false,true,false,false]],
+               [false,true,false,false],
+               // Bild
+               ""],
 
                [// Frage 6
                "Während des Löschangriffs (Außenangriff) entdecken Sie eine leblose Person. Welchen der folgenden Schritte führen Sie zusammen mit ihrem Truppmann im Rahmen der Patientenkontrolle aus Sicht der Ersten Hilfe vorrangig durch?",
@@ -68,7 +78,9 @@ var fragen = [[// Frage 1
                "Zuerst Blutung stillen, dann Atmung kontrollieren",
                // Antwort 4
                "Zuerst Feuer löschen, dann Verletzten betreuen",
-               [true,false,false,false]],
+               [true,false,false,false],
+               // Bild
+               ""],
 
                [// Frage 7
                "Welche Aussagen über Kohlendioxid sind zutreffend?",
@@ -80,7 +92,9 @@ var fragen = [[// Frage 1
                "leichter als Luft",
                // Antwort 4
                "steuert die Atmung, ist bei zu hoher Konzentration giftig",
-               [false,false,false,true]],
+               [false,false,false,true],
+               // Bild
+               ""],
 
                [// Frage 8
                "Welche Aussagen über Kohlendioxid sind zutreffend?",
@@ -92,10 +106,12 @@ var fragen = [[// Frage 1
                "ungifig",
                // Antwort 4
                "entsteht bei vollkommener Verbrennung",
-               [true,true,false,true]],
+               [true,true,false,true],
+               // Bild
+               ""],
 
                [// Frage 9
-               "Welche Gefahren entstehen in dieser Situation? (Bild zeigt: Feuerwehrmänner tragen Pumpe in Richtung eines Flusses (Gelender vor ihnen))",
+               "Welche Gefahren entstehen in dieser Situation?",
                // Antwort 1
                "Ausrutschen",
                // Antwort 2
@@ -104,7 +120,9 @@ var fragen = [[// Frage 1
                "Quetschen",
                // Antwort 4
                "Stürzen",
-               [true,true,true,true]],
+               [true,true,true,true],
+               // Bild
+               "Bild1.png"],
 
                [// Frage 10
                "Kreuzen Sie die zur GAMS-Regel gehörenden Begriffe an",
@@ -116,7 +134,9 @@ var fragen = [[// Frage 1
                "Alarmieren",
                // Antwort 4
                "Grundsätze erklären",
-               [false,true,false,false]],
+               [false,true,false,false],
+               // Bild
+               ""],
 
                [// Frage 11
                "Kreuzen Sie die zur GAMS-Regel gehörenden Begriffe an",
@@ -128,7 +148,9 @@ var fragen = [[// Frage 1
                "Gefahr erkennen",
                // Antwort 4
                "Menschenrettung",
-               [true,false,true,true]],
+               [true,false,true,true],
+               // Bild
+               ""],
 
                [// Frage 12
                "Sie erhalten als Truppführer den Auftrag, eine Saugleitung aufzubauen. Warum soll in fließenden Gewassern der Saugkorb im Regelfall gegen die Fließrichtung des Wassers liegen? Kreuzen Sie die richtigen Antworten an.",
@@ -140,7 +162,9 @@ var fragen = [[// Frage 1
                "Die Saugleitung kann in dieser Stellung besser gehalten werden",
                // Antwort 4
                "Damit der Schmutz am Saugkorb leichter weggespült wird",
-               [true,true,false,false]],
+               [true,true,false,false],
+               // Bild
+               ""],
 
                [// Frage 13
                "Worauf achten Sie bei der Entnahme von Geräten aus den Fahrzeug-Geräteräumen?",
@@ -152,7 +176,9 @@ var fragen = [[// Frage 1
                "Dass die Aufstiegshilfen (Haltegriffe, Trittbretter, Leitern usw.) benutzt werden",
                // Antwort 4
                "Auf Verletzungsgefahren",
-               [false,true,true,true]],
+               [false,true,true,true],
+               // Bild
+               ""],
 
                [// Frage 14
                "Welche der folgenden Feuerlöscher können bei Bränden fester Stoffe wirkungsvoll eingesetzt werden?",
@@ -164,7 +190,9 @@ var fragen = [[// Frage 1
                "Pulverlöscher mit Glutbrandpulver",
                // Antwort 4
                "Pulverlöscher mit Flammbrandpulver",
-               [false,false,true,false]],
+               [false,false,true,false],
+               // Bild
+               ""],
 
                [// Frage 15
                "Welche der folgenden Feuerlöscher können bei Bränden fester Stoffe wirkungsvoll eingesetzt werden?",
@@ -176,7 +204,9 @@ var fragen = [[// Frage 1
                "Schaumlöscher",
                // Antwort 4
                "Pulverlöscher mit Glutbrandpulver",
-               [false,true,true,true]],
+               [false,true,true,true],
+               // Bild
+               ""],
 
                [// Frage 16
                "Welche der folgenden Feuerlöscher können bei Bränden flüssiger Stoffe wirkungsvoll eingesetzt werden?",
@@ -188,7 +218,9 @@ var fragen = [[// Frage 1
                "Pulverlöscher mit ABC-Pulver",
                // Antwort 4
                "Nasslöscher",
-               [true,false,true,false]],
+               [true,false,true,false],
+               // Bild
+               ""],
 
                [// Frage 17
                "Welche der folgenden Feuerlöscher können bei Bränden flüssiger Stoffe wirkungsvoll eingesetzt werden?",
@@ -200,7 +232,9 @@ var fragen = [[// Frage 1
                "Schaumlöscher",
                // Antwort 4
                "Kolendioxidlöscher",
-               [true,false,true,true]],
+               [true,false,true,true],
+               // Bild
+               ""],
 
                [// Frage 18
                "Was beachten Sie als Truppführer bei der Brandbekampfung?",
@@ -212,7 +246,9 @@ var fragen = [[// Frage 1
                "Wasserabgabe immer wieder stoppen, damit der Wasserdampf abziehen kann und die Entwicklung der Löscharbeiten kontrolliert werden kann",
                // Antwort 4
                "Ausreichende Schlauchreserve",
-               [false,true,true,true]],
+               [false,true,true,true],
+               // Bild
+               ""],
 
                [// Frage 19
                "Was beachten Sie als Truppführer bei der Brandbekampfung?",
@@ -224,7 +260,9 @@ var fragen = [[// Frage 1
                "Gegen den Wind angreifen",
                // Antwort 4
                "",
-               [false,true,false,false]],
+               [false,true,false,false],
+               // Bild
+               ""],
 
                [// Frage 20
                "Sie bekommen den Auftrag eine Fahrzeugbatterie abzuklemmen. Welchen Pol klemmen Sie zuerst ab?",
@@ -236,7 +274,9 @@ var fragen = [[// Frage 1
                "Bei modernen Fahrzeugen ist die Reihenfolge egal",
                // Antwort 4
                "Darf von der Feuerwehr nicht abgeklemmt werden",
-               [true,false,false,false]],
+               [true,false,false,false],
+               // Bild
+               ""],
 
                [// Frage 21
                "Bei einem Unfall laufen Betriebsstoffe aus. Womit können Sie kleinere Mengen ausgelaufenen Treibstoffs abdecken?",
@@ -248,7 +288,9 @@ var fragen = [[// Frage 1
                "Erde",
                // Antwort 4
                "Sagespäne",
-               [false,true,true,true]],
+               [false,true,true,true],
+               // Bild
+               ""],
 
                [// Frage 22
                "Worauf ist beim Aufstellen einer Steckleiter zu achten?",
@@ -260,7 +302,9 @@ var fragen = [[// Frage 1
                "Dass immer alle 4 Leiterteile genutzt werden",
                // Antwort 4
                "Dass 3 Sprossen bzw. 1 m Überstand eingehalten werden, wenn keine andere Möglichkeit zum Festhalten besteht",
-               [true,true,false,true]],
+               [true,true,false,true],
+               // Bild
+               ""],
 
                [// Frage 23
                "Worauf ist beim Aufstellen einer Steckleiter zu achten?",
@@ -272,7 +316,9 @@ var fragen = [[// Frage 1
                "Dass der Untergrund waagrecht und tragfähig ist",
                // Antwort 4
                "Dass ein Anstellwinkel von ca. 85° eingehalten wird",
-               [false,true,true,false]],
+               [false,true,true,false],
+               // Bild
+               ""],
 
                [// Frage 24
                "Welche taktischen Einsatzgrundsätze beachten Sie als Truppführer?",
@@ -284,7 +330,9 @@ var fragen = [[// Frage 1
                "Truppweise geschlossen vorgehen",
                // Antwort 4
                "Rückzugsweg sichern",
-               [true,true,true,true]],
+               [true,true,true,true],
+               // Bild
+               ""],
 
                [// Frage 25
                "Welche taktischen Einsatzgrundsätze beachten Sie als Truppführer?",
@@ -296,7 +344,9 @@ var fragen = [[// Frage 1
                "Immer kriechend vorgehen",
                // Antwort 4
                "Löschstrahl nicht unterbrechen",
-               [true,true,false,false]],
+               [true,true,false,false],
+               // Bild
+               ""],
 
                [// Frage 26
                "Der Wassertrupp bekommt den Auftrag, ein Gebäude (rauchfrei) nach Personen zu durchsuchen. Wer legt die Reihenfolge der Absuche fest, wenn im Befehl des Gruppenführers kein Hinweis gegeben wurde?",
@@ -308,7 +358,9 @@ var fragen = [[// Frage 1
                "Der Truppmann",
                // Antwort 4
                "Der Maschinist",
-               [false,true,false,false]],
+               [false,true,false,false],
+               // Bild
+               ""],
 
                [// Frage 27
                "Der Einheitsführer befiehlt, dass Sie mit lhrem Trupp den nächsten Hydranten als Wasserversorgung einsatzbereit machen sollen. In etwa 25 m Entfernung zur Einsatzstelle entdecken Sie einen Unterflurhydranten, auf dem das im Bild dargestellte Fahrzeug parkt. In 100 m Entfernung sehen Sie einen Überflurhydranten <br> - wie entscheiden Sie sich?",
@@ -320,7 +372,9 @@ var fragen = [[// Frage 1
                "Ich weise die Polizei an, den Fahrzeughalter ausfindig zu machen und das Entfernen des Fahrzeuges zu veranlassen",
                // Antwort 4
                "",
-               [false,true,false,false]],
+               [false,true,false,false],
+               // Bild
+               "Bild2.png"],
 
                [// Frage 28
                "Beim Überqueren von Straßen mit Schlauchleitungen sind mindestens drei Schlauchbrücken auf einer Fahrbahnseite so auszulegen, dass Fahrzeuge verschiedener Spurbreite (PKW/LKW) die Schlauchleitung überfahren können. Der Truppführer achtet darauf, dass die Schlauchbrücken fachgerecht verlegt werden. Wie groß wählen sie den Abstand?",
@@ -332,7 +386,9 @@ var fragen = [[// Frage 1
                "Etwa 1,5 m",
                // Antwort 4
                "Etwa 3,0 m",
-               [false,true,false,false]],
+               [false,true,false,false],
+               // Bild
+               "Bild3.png"],
 
                [// Frage 29
                "Der Truppführer ist für die genaue Lage des Verteilers verantwortlich. Wo sollten Sie nach Möglichkeit den Verteiler nicht ablegen?",
@@ -344,7 +400,9 @@ var fragen = [[// Frage 1
                "Außerhalb des Fluchtwegs",
                // Antwort 4
                "",
-               [true,true,false,false]],
+               [true,true,false,false],
+               // Bild
+               ""],
 
                [// Frage 30
                "Als Truppführer sind Sie für die Sicherheit des Trupps verantwortlich. Welche Grundsätze beachten Sie dabei beim Auftreten atomarer Gefahren?",
@@ -356,7 +414,9 @@ var fragen = [[// Frage 1
                "Aufenthaltsdauer begrenzen",
                // Antwort 4
                "Inkorporation ausschließen",
-               [true,true,true,true]],
+               [true,true,true,true],               
+               // Bild
+               ""],
             ];
 
 var index = 0;
@@ -389,6 +449,12 @@ function setFields(){
     } else{
         document.getElementById("checkbox4").hidden = true;
         document.getElementById("checkbox4Label").hidden = true;
+    }
+    if(fragen[index][6] != ""){
+      document.getElementById("Bild").hidden = false;
+      document.getElementById("Bild").src = fragen[index][6];
+    }else{
+      document.getElementById("Bild").hidden = true;
     }
 }
 
